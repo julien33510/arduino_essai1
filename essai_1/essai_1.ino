90a5fbf871f972e662b8219b6ec34c524082e179
julien33510 <120409256+julien33510@users.noreply.github.com>
2023-09-11 17:32:14 +0200

    Modif Variable pin

--- essai_1/essai_1.ino
+++ essai_1/essai_1.ino
@@ -64,8 +64,8 @@ void card_desactivate() {
 
 void setup() {
   // Clock 4MHz
-  pinMode(3, OUTPUT);
-  //pinMode(11, OUTPUT);
+  //pinMode(TX, OUTPUT);
+  //pinMode(CLK, OUTPUT);
   TCCR2A = _BV(COM2A0) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
   TCCR2B = _BV(WGM22) | _BV(CS20);
   OCR2A = 1;
